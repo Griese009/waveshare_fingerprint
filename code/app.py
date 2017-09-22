5ec7efe6f81cd428c241c17894c84c18701dd0fb
Griese009 <32206327+Griese009@users.noreply.github.com>
2017-09-22 23:06:40 +0200

    Added some code
    Can difference between special commands

--- code/app.py
+++ code/app.py
@@ -32,7 +32,7 @@ class Reader:
                                        0x00,  # Null
                                        0x00,  # CHK
                                        0xF5],
-                        "get_fingerprint": [0xF5, 0x24, 0x00, 0x00, 0x00, 0x00, 0x24, 0xF5],
+                        "get_fingerprint": [0xF5, 0x24, 0x00, 0x00, 0x00, 0x00, 0x24, 0xF5],  # Not implemented yet
                         "delete_all": [0xF5, 0x05, 0x00, 0x00, 0x00, 0x00, 0x05, 0xF5],
                         "delete_user": [0xF5,
                                         0x04,  # Command code
@@ -65,12 +65,20 @@ class Reader:
                                       0x00,  # Privileges (0x01, 0x02, 0x03)
                                       0x00,  # Null
                                       0x00,  # CHK
-                                      0xF5]]}
+                                      0xF5]],
+                        "acquire_priviliege": [0xF5,
+                                               0x0A,  # Command
+                                               0x00,  # User-ID 1st Byte
+                                               0x00,  # User-ID 2nd Byte
+                                               0x00,  # None
+                                               0x00,  # None
+                                               0x00,  # CHK
+                                               0xF5]}
 
     def open_connection(self):
         try:
-            if self.ser == None:
-                for port in self.port:
+            if self.ser is None:
+                for port in self.port:  # just open the serial port if possible
                     try:
                         self.ser = serial.Serial(port=port, baudrate=self.baudrate, bytesize=self.bytemode)
                         return True
@@ -84,7 +92,7 @@ class Reader:
 
     def close_connection(self):
         try:
-            if self.ser == None:
+            if self.ser is None:  # just closing the connection
                 pass
             else:
                 self.ser.close()
@@ -94,60 +102,60 @@ class Reader:
             print('Die Verbindung konnte aufgrund von: "' + str(e) + '" nicht geschlossen werden.')
             return False
 
-    def sende_befehl_mit_nutzer(self, befehl, user=None, privileges=None):
-        if user is None:
+    def send_command(self, command, user=None, privileges=None):
+        if user is None:  # adjust command for the sensor if not set
             user = [0x00, 0x00]
         else:
             pass
 
-        if privileges is None:
+        if privileges is None:  # adjust command for the sensor if not set
             privileges = 0x00
         else:
             pass
 
-        assert isinstance(privileges, int)
+        assert isinstance(privileges, int)  # controll values if these are correct
         assert isinstance(user[0], int)
         assert isinstance(user[1], int)
-        assert isinstance(befehl, str)
+        assert isinstance(command, str)
         try:
-            if befehl == "add_user":
-                for i in self.befehle[befehl]:
+            if command == "add_user":  # add_user is a special command
+                for i in self.befehle[command]:
                     if self.ser is None:
                         self.open_connection()
                         a = i
-                        self.ser.write([a[0], a[1], user[0], user[1], privileges, a[5],
-                                a[1] ^ user[0] ^ user[1] ^ privileges ^ a[5], a[7]])
-                        if self.daten_erhalten():
+                        self.ser.write([a[0], a[1], user[0], user[1], privileges, a[5],  # rebuild command
+                                a[1] ^ user[0] ^ user[1] ^ privileges ^ a[5], a[7]])  # and do the XOR-Operation
+                        if self.receive_data(command):
                             pass
                         else:
                             self.close_connection()
                             return False
                         self.close_connection()
                     else:
-                        a = self.befehle[befehl]
-                        self.ser.write([a[0], a[1], user[0], user[1], privileges, a[5],
+                        a = self.befehle[command]
+                        self.ser.write([a[0], a[1], user[0], user[1], privileges, a[5],  # see previous comment
                                 a[1] ^ user[0] ^ user[1] ^ privileges ^ a[5], a[7]])
-                        if self.daten_erhalten():
+                        if self.receive_data(command):
                             pass
                         else:
                             return False
             else:
                 if self.ser is None:
                     self.open_connection()
-                    a = self.befehle[befehl]
-                    self.ser.write([a[0], a[1], user[0], user[1], privileges, a[5],
+                    a = self.befehle[command]
+                    self.ser.write([a[0], a[1], user[0], user[1], privileges, a[5],  # see previous comment
                                 a[1] ^ user[0] ^ user[1] ^ privileges ^ a[5], a[7]])
-                    if self.daten_erhalten():
+                    if self.receive_data(command):
                         pass
                     else:
                         self.close_connection()
                         return False
                     self.close_connection()
                 else:
-                    a = self.befehle[befehl]
-                    self.ser.write([a[0], a[1], user[0], user[1], privileges, a[5],
+                    a = self.befehle[command]
+                    self.ser.write([a[0], a[1], user[0], user[1], privileges, a[5],  # see previous comment
                                 a[1] ^ user[0] ^ user[1] ^ privileges ^ a[5], a[7]])
-                    if self.daten_erhalten():
+                    if self.receive_data(command):
                         pass
                     else:
                         return False
@@ -156,14 +164,43 @@ class Reader:
             print(e)
             return False
 
-    def daten_erhalten(self):
+    def receive_data(self, command):
+        assert isinstance(command, str)
+        print(command)
         try:
-            a = self.ser.read(8)
-            b = str(a).split("\\")
-            if b[5] == "x00":  # Check if command was succesfull else return False
-                pass
-            elif b[5] == "x01" or b[5] == "x05" or b[5] == "x07" or b[5] == "x08" or b[5] == "x04":
-                return False
+            a = self.ser.read(8)  # receive data from sensor
+            b = str(a).split("\\")  # reformat these data and split them
+            if command != "acquire_priviliege" and command != "ask_1_to_N":
+                if b[5] == "x00":  # Check if command was succesfull else return False
+                    # the fifth byte shows if the command was successfull or not
+                    pass
+                elif b[5] == "x01" or b[5] == "x05" or b[5] == "x07" or b[5] == "x08" or b[5] == "x04":
+                    if b[5] == "x01":
+                        print("Operation failed")
+                    elif b[5] == "x04":
+                        print("Fingerprint database is full")
+                    elif b[5] == "x05":
+                        print("No such user")
+                    elif b[5] == "x07":
+                        print("User already exists")
+                    elif b[5] == "x08":
+                        print("Acquisition timeout")
+                    return False
+            elif command == "acquire_priviliege":
+                if b[5] == "x01":
+                    print("Admin")
+                elif b[5] == "x02":
+                    print("User")
+                elif b[5] == "x03":
+                    print("Guest")
+                else:
+                    return False
+            else:
+                if b[5] == "x05":
+                    print("No such user")
+                    return False
+                elif b[5] == "x08":
+                    print("Acquisition timeout")
             return True
         except Exception as e:
             print(e)
@@ -179,22 +216,27 @@ class Reader:
             print("Close connection: ok")
         else:
             print("Close connection: failed")
-        if self.sende_befehl_mit_nutzer(befehl="ask_1_to_N"):
+        if self.send_command(command="ask_1_to_N"):
             print("Read fingerprint 1 to N: ok")
         else:
             print("Read fingerprint 1 to N: failed")
-        if self.sende_befehl_mit_nutzer(befehl="add_user", user=[0x00, 0x05], privileges=0x01):
+        if self.send_command(command="add_user", user=[0x00, 0x05], privileges=0x01):
             print("Add User: ok")
         else:
             print("Add user: failed")
-        if self.sende_befehl_mit_nutzer(befehl="delete_user", user=[0x00, 0x05]):
+        if self.send_command(command="delete_user", user=[0x00, 0x05]):
             print("User deleted: ok")
         else:
             print("User deleted: failed")
-        if self.sende_befehl_mit_nutzer(befehl="ask_1_to_1", user=[0x00, 0x01]):
+        if self.send_command(command="ask_1_to_1", user=[0x00, 0x01]):
             print("Read fingerprint 1 to 1: ok")
         else:
             print("Read fingerprint 1 to 1: failed")
+
+        if self.send_command(command="acquire_priviliege", user=[0x00, 0x01]):
+            print("success")
+        else:
+            print("error")
 
 
 if __name__ == "__main__":
